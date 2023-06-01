8ff782ce2a69b8c4bf19fa8233d53424cf64f1cc
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-06-01 13:53:46 +0530

    Remove next.config.js from the repository

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        API_URL: 'http://localhost:8000',
-    }
-}
-
-module.exports = nextConfig
