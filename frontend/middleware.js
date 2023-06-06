77f11ab3961cdacd0eccc07867b6bf24484b63f2
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-06-06 20:44:06 +0530

    resolved the middleware issue with allowedParams additional keywords

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,29 +1,34 @@
 import { NextResponse } from "next/server";
 
 const allowedParams = [
-    "keyword",
-    "location",
-    "page",
-    "education",
-    "experience",
-    "salary",
-    "jobType",
+  "keyword",
+  "location",
+  "page",
+  "education",
+  "experience",
+  "salary",
+  "jobType",
+  "url",
+  "q",
+  "w",
+  "id",
 ];
 
 export async function middleware(req) {
-    const url = req.nextUrl;
-    let changed = false;
-    console.log(url);
+  // const country = req.geo.country
 
-    url.searchParams.forEach((param, key) => {
-        if (!allowedParams.includes(key)) {
-            url.searchParams.delete(key);
-            changed = true;
-        }
-    });
+  const url = req.nextUrl;
+  let changed = false;
 
-    if (changed) {
-        return NextResponse.redirect(url);
+  url.searchParams.forEach((param, key) => {    
+    if (!allowedParams.includes(key)) {
+        url.searchParams.delete(key);
+        changed = true;
     }
+    
+  });
 
+  if (changed) {
+    return NextResponse.redirect(url);
+  }
 }
