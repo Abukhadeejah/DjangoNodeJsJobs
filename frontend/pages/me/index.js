381776b804071d9235637c16f550da16da2e4761
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-06-07 06:13:52 +0530

    User Profile page updated

---
+++ frontend/pages/me/index.js
@@ -0,0 +1,14 @@
+import Layout from '../../components/layout/Layout';
+import UpdateProfile from '../../components/user/UpdateProfile';
+
+
+export default function UpdateProfilePage() {
+
+  return (
+    <Layout>
+      <UpdateProfile title="Update User Profile"/>
+    </Layout>
+    
+  )
+}
+
